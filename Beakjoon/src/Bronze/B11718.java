f085ac5535e12868cc5a6016c5da55f7642b3eb4
jiggy97 <dkswnsdud159@naver.com>
2023-05-03 15:51:10 +0900

    [java] Beakjoon - [bronze] 11718

---
+++ Beakjoon/src/Bronze/B11718.java
@@ -0,0 +1,14 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class B11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String s = sc.nextLine();
+            System.out.printf(s);
+        }
+    }
+}
