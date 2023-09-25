09fd6591bdf63d8ccc78f9cf9ba26bf8b37413c3
jiggy97 <dkswnsdud159@naver.com>
2023-09-26 00:41:34 +0900

    [java] Beakjoon - [silver] 2941

--- Beakjoon/src/Silver/S2941.java
+++ Beakjoon/src/Silver/S2941.java
@@ -1,17 +1,44 @@
 package Silver;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class S2941 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String word = sc.nextLine();
+        String[] words = word.split("");
         int count = 0;
-        String[] croatia = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
-        for (String c : croatia) {
-            if(word.contains(c)) {
+        List<String> croatia = List.of(new String[]{"c=", "c-", "d-", "lj", "nj", "s=", "z="});
+
+        for (int i = 0; i < words.length; i++) {
+            if (i == words.length - 1) {
                 count++;
+                break;
+            } else if (i == words.length - 2) {
+                String w1 = words[i] + words[i+1];
+                if (croatia.contains(w1)) {
+                    count++;
+                    break;
+                }
+                count++;
+                continue;
             }
+            String w1 = words[i] + words[i+1];
+            String w2 = words[i] + words[i+1] + words[i+2];
+
+            if (croatia.contains(w1)) {
+                count ++;
+                i++;
+                continue;
+            }
+
+            if (w2.equals("dz=")) {
+                count++;
+                i += 2;
+                continue;
+            }
+            count++;
         }
         System.out.println(count);
     }
